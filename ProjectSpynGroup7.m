9ec0633955ecc35dde491bb1ed9f5119da746228
Urabite <pachotalia@gmail.com>
2025-10-28 16:25:48 -0700

    Update ProjectSpynGroup7.m

--- ProjectSpynGroup7.m
+++ ProjectSpynGroup7.m
@@ -7,22 +7,24 @@ left motor:  B      % Motor port A = left wheel
 right motor: C      % Motor port D = right wheel
 %}
 
-lwheel = 50; %need to edit this 
-rwheel = 50; %need to edit this 
+lwheel = -50; %need to edit this 
+rwheel = -50; %need to edit this 
+lbwheel = 20;
+rbwheel = 20;
 threshhold = 50;
 
-brick.SetColorMode(2,2);
+%brick.SetColorMode(2,2);
 
-while 1
+while true
   brick.MoveMotor('B',lwheel);
   brick.MoveMotor('C',rwheel);
 
   touch = brick.TouchPressed('A');
-  color = brick.ColorCode('D');
+  %color = brick.ColorCode('D');
   distance = brick.UltrasonicDist(2); 
 
-  if distance > threshold % if the right wall disappears
-      pause(0.5); % this makes it wait to get past wall 
+  if distance > threshold %if the right wall disappears
+      pause(0.5); %this makes it wait to get past wall 
       brick.stopMotor('BC', 'Brake');
       brick.moveMotor('B', -20);
       pause(2.3); %turning time, which we can change as needed
@@ -35,3 +37,14 @@ while 1
     pause(1);
     brick.StopMotor('BC', 'Brake');
     dist = brick.UltrasonicDist(2);
+    brick.MoveMotor('B', lbwheel);
+    brick.MoveMotor('C', rbwheel);
+    pause(1);
+    brick.StopMotor('BC', 'Brake');
+    brick.MoveMotor('C', -20);
+    pause(2.3); %turning time, which we can change as needed
+    brick.StopMotor('C',lwheel);
+    brick.MoveMotor('B',lwheel);
+    brick.MoveMotor('C',rwheel);
+    pause(2);
+  end
